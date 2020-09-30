480f349c1638ced0ce739413d7ce84239cf36c43
mifka <radimmifka@gmail.com>
2020-09-30 20:31:20 +0200

    edit message

--- message.py
+++ message.py
@@ -1,10 +1,13 @@
 message_text = """
 Dobrý den,
 
-měli bychom zájem o vaši zakázku. Jsme tým dvou studentů IT oboru a rádi bychom si přivydělali a získali cenné zkušenosti.
-Máme zkušenosti s jazyky jako jsou HTML, Javascript, CSS ale i Python, PHP či MySQL.
+měli bychom zájem o vaši zakázku. Jsme tým dvou studentů oboru IT a rádi bychom si přivydělali a získali cenné zkušenosti.
+
+Z webového vývoje ovládáme jazyky jako jsou [B]PHP, JavaScript, Bootstrap, MySQL [/B] a samozřejmě [B]HTML + CSS.[/B] Čili jsme schopni vytvořit web i s databází.
+Aplikace a skripty děláme v Pythonu. [B]Python[/B] je nejspíš naše největší přednost, takže jsme v něm schopni dělat desktopové aplikace, screen-scraping, web-scraping, 2D hry, různé boty nebo i pomůcky pro automatizaci každodenních činností ze strany uživatele.
 
 Pokud o nás máte zájem, určitě se ozvěte.
+
 S pozdravem,
 Kazda Antonín
 """
