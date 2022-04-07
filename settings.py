cdd476592ec2f4024955539c44cfd8c38148eb4f
Radim Mifka <radimmifka@gmail.com>
2022-04-07 22:43:24 +0200

    removed unnecessary file

--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# LIVE VERSION
-ACCESS_TOKEN = str(os.environ.get('ACCESS_TOKEN'))
-USERNAME = str(os.environ.get('username'))
-PASSWORD = str(os.environ.get('password'))
-MESSAGE = str(os.environ.get('message'))
-
