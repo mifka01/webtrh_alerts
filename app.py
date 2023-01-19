254b6b78e5ad8ffcffff45d228469298353861d3
Radim Mifka <radimmifka@gmail.com>
2023-01-19 19:16:58 +0100

    Add discord integration

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from Alert import Alert
-from test_dotenv import valid_environment
+from discord import Intents
+from valid_dotenv import valid_environment
 from sys import exit
+from modules.discord_client import DClient
+from config import DISCORD_TOKEN
 
 
 if __name__ == "__main__":
-    print('[info] Starting webtrh alerts')
-    print('[info] Testing enviroment')
+    print('[info] Starting WT Alerts')
+    print('[info] Checking Enviroment settings ')
     if (valid_environment()):
         print('[info] Running...')
         print('--------------------')
-        Alert().run()
+        client = DClient(intents=Intents.default())
+        client.run(DISCORD_TOKEN)
     exit(1)
